32afd12c2c34bc564f70d0cffdc3a7617a9f415d
Omer Salkanovic <osalkanovic5@gmail.com>
2021-12-19 17:48:11 +0100

    fix(tsconfig): tsconfig for package

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import configStore from './config/config';
-import provider from './provider/Provider';
-import { UserContract } from './test/contract';
+//import configStore from './config/config';
+//import provider from './provider/Provider';
+export { Contract } from './decorators/contract';
+export { Column } from './decorators/column';
+export { InjectContract } from './decorators/inject-contract';
 
-import { Baci, User } from './test/user.contract';
+// const bootstap = async () => {
+//   await configStore.readConfig();
 
-const bootstap = async () => {
-  await configStore.readConfig();
-  const user = new User();
-  const baci = new Baci();
+//   await provider.loadContracts();
+// };
 
-  await provider.loadContracts();
-  const a = new UserContract();
-  // const b = await a.createUser();
-  // await a.updateById('1e043bc5-b74c-4c49-859d-085ede09f68e', {
-  //   name: 'Omer',
-  //   lastName: 'tito',
-  // });
-  // const c = await a.getAll();
-  //console.log(c);
-};
-
-bootstap();
+// bootstap();
